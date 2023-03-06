644c51942daf0111bd6bb36c70576f72116db7c8
asherk7 <masherk03@hotmail.com>
2023-03-06 15:38:12 -0500

    replaced old generator with new one

--- generator/src/main/java/Main.java
+++ generator/src/main/java/Main.java
@@ -1,80 +1,24 @@
-import ca.mcmaster.cas.se2aa4.a2.generator.DotGen;
+import ca.mcmaster.cas.se2aa4.a2.generator.adt.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.generator.configuration.Configuration;
+import ca.mcmaster.cas.se2aa4.a2.generator.export.enricher.RandomEnricher;
+import ca.mcmaster.cas.se2aa4.a2.generator.export.Exporter;
+import ca.mcmaster.cas.se2aa4.a2.generator.specification.Buildable;
+import ca.mcmaster.cas.se2aa4.a2.generator.specification.SpecificationFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
-import java.util.Scanner;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        DotGen generator = new DotGen();
-        Scanner my_Scanner = new Scanner(System.in);
-        Mesh myMesh;
-        int num_of_polygons = 50;
-        int relaxation_level = 1000;
-        System.out.println("Enter command -H for help with other commands");
-        if (contains("-H", args)){
-            System.out.println("""
-            Commands
-            --------
-            -N, number of polygons
-            Users will be able to determine how many polygons they wish to add to the irregular mesh
-
-            -RL, number of relaxation levels
-            Users will be able to determine the number of levels the voronoi diagram will pass through
-
-            -IR, irregular mesh
-            By default, the regular mesh will be selected. To switch, type said command
-
-            """);
+        Configuration config = new Configuration(args);
+        Buildable specification = SpecificationFactory.create(config);
+        Mesh theMesh = specification.build();
+        Structs.Mesh exported = new Exporter().run(theMesh);
+        if(config.export().containsKey(Configuration.DEMO)) {
+            exported = new RandomEnricher(0.2f).process(exported);
         }
-
-        if (contains("-N", args)) {
-            System.out.println("How many polygons do you wish to enter? ");
-            String result1 = my_Scanner.nextLine();
-            try{
-                num_of_polygons = Integer.valueOf(result1);
-            } catch (Exception e){
-                System.out.println("Not an integer! Default will be used");
-            }
-        }
-
-        if (contains("-RL", args)){
-            System.out.println("How many relaxation levels do you wish to enter? ");
-            String result2 = my_Scanner.nextLine();
-            try{
-                relaxation_level = Integer.valueOf(result2);
-            } catch (Exception e){
-                System.out.println("Not an integer! Default will be used");
-            }
-
-        }
-
-        if (contains("-IR", args)){
-            myMesh =  generator.generateVoronoi(num_of_polygons, relaxation_level);
-        } else{
-            myMesh = generator.generate();
-        }
-
-        MeshFactory factory = new MeshFactory();
-        factory.write(myMesh, args[0]);
-    }
-
-    public static boolean contains(String compare, String[] args){
-        for (int i = 0; i < args.length; i++){
-            if (compare.equals(args[i])){
-                return true;
-            }
-        }
-        return false;
+        new MeshFactory().write(exported, config.export(Configuration.FILENAME));
     }
 }
-
-/*
- CLI Args:
-    - Kind of mesh: grid or irregular DONE
-    - Number of polygons: Done
-    - Relaxation level
-    - Help command
- 
- */
