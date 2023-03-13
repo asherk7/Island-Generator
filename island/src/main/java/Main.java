ce6a56ba35dd5bd1a88c74b077870e2b16a6d10c
Mehdi Syed <m3hd1.syed@Mehdis-MacBook-Air.local>
2023-03-13 16:57:37 -0400

    Acces mesh through CLA

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -1,6 +1,13 @@
+import java.io.IOException;
+
+import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+
 public class Main {
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
+        //First argument will be the input filename
+        Structs.Mesh aMesh = new MeshFactory().read(args[0]);
         
     }
 
