99a85e77aa60bf0d14e878d373014dbacb49f40f
Mehdi Syed <m3hd1.syed@Mehdis-MacBook-Air.local>
2023-03-13 17:32:04 -0400

    Completed makeSegments & makeVertices functions

--- island/src/main/java/remakeMesh.java
+++ island/src/main/java/remakeMesh.java
@@ -1,5 +1,8 @@
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex.Builder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,19 +25,28 @@ public class remakeMesh {
         for (Polygon p: polygonList){
             Polygon.Builder polygon = Polygon.newBuilder();
             //neighbour, centroid, segments
+            
         }
 
         newMesh.addAllPolygons(newPolygons);
     }
 
     public void makeSegments(Mesh aMesh, Mesh.Builder newMesh){
-
-        newMesh.addAllSegments();
+        List<Segment> newSegmentList = new ArrayList<>();
+        List<Segment> segmentList = aMesh.getSegmentsList();
+        for (Segment s : segmentList){
+            newSegmentList.add(Segment.newBuilder().setV1Idx(s.getV1Idx()).setV2Idx(s.getV2Idx()).build());
+        }
+        newMesh.addAllSegments(newSegmentList);
     }
 
     public void makeVertices(Mesh aMesh, Mesh.Builder newMesh){
-
-        newMesh.addAllVertices();
+        List<Vertex> newVertexList = new ArrayList<>();
+        List<Vertex> vertexList = aMesh.getVerticesList();
+        for (Vertex v : vertexList){
+            newVertexList.add(Vertex.newBuilder().setX(v.getX()).setY(v.getY()).build());
+        }
+        newMesh.addAllVertices(newVertexList);
     }
 
 }
