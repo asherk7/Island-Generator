ea48ee311495a7f4b285cec603a6224c15be1888
Mehdi Syed <m3hd1.syed@Mehdis-MacBook-Air.local>
2023-03-13 16:00:03 -0400

    remake mesh class updated

--- island/src/main/java/remakeMesh.java
+++ island/src/main/java/remakeMesh.java
@@ -1,3 +1,13 @@
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+
 public class remakeMesh {
-    
+
+    public Mesh newMeshBuilder(Mesh aMesh){
+        
+        Mesh.Builder newMesh = Mesh.newBuilder();
+        
+        
+        return aMesh;
+    }
 }
