ffde582cd6e840a01fad2e2060aa182ab1b98054
Mehdi Syed <m3hd1.syed@Mehdis-MacBook-Air.local>
2023-03-18 21:11:40 -0400

    Created Hills profile

---
+++ island/src/main/java/ElevationProfiles/Hills.java
@@ -0,0 +1,13 @@
+package ElevationProfiles;
+
+//import java.util.ArrayList;
+import java.util.List;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+//import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
+
+public class Hills implements AltProfile{
+    public void markHeight(List<Structs.Polygon.Builder> islandPolygonList, List<Structs.Polygon.Builder> polygonList){
+
+    }
+}
